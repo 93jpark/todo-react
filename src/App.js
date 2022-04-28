c27818eef7dc9c61ffb382001b450486eb077f2f
Joonwoo Park <93jpark@gmail.com>
2022-04-28 18:58:34 +0900

    implements basic todo with materialui

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Todo from './Todo';
+import { Paper, List } from "@mui/material"
 
-function App() {
+const App = () => {
+
+  const items = [
+        {id:0, title:"Hello World 1", done:true},
+        {id:1, title: "Hello World 2", done: false},
+      ];
+  
+  
+  const todoItems = items.length > 0 && (
+    <Paper style={{margin:16}}>
+      <List>
+        {items.map((item, idx)=> (
+            <Todo item={item} key={item.id} />
+          )
+        )}
+      </List>
+    </Paper>
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='App'>
+        {todoItems}
+      </div>
+    </>
   );
 }
 
