b3c2a74a51b510150797ee89cd3ff04afc5aa152
Joonwoo Park <93jpark@gmail.com>
2022-06-07 19:16:03 +0900

    implements login page with signin api service function

--- src/Login.js
+++ src/Login.js
@@ -1,10 +1,67 @@
+import { TextField, Button, Grid, Typography, Container } from '@mui/material';
 import React from 'react';
+import { signin } from "./service/ApiService";
 
 const Login = () => {
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const data = new FormData(event.target);
+        const email = data.get("email");
+        const password = data.get("password");
+        // ApiService의 signin() 메소드를 이용해 로그인
+        signin({ email:email, password:password })
+    }
+
     return (
-        <p>
-            로그인 페이지
-        </p>
+        <Container component="main" maxWidth="xs" style={{marginTop : "8%"}}>
+            <Grid container spacing={2}>
+                <Grid item xs={12}>
+                    <Typography component="h1" variant="h5">
+                        로그인
+                    </Typography>
+                </Grid>
+            </Grid>
+            <form noValidate onSubmit={handleSubmit}>
+                {" "}
+                {/* submit 버튼을 클릭하면 handleSubmit 호출 */}
+                <Grid container spacing="2">
+                    <Grid item xs={12}>
+                        <TextField 
+                            variant="outlined"
+                            required
+                            fullWidth
+                            id="email"
+                            label="이메일 주소"
+                            name="email"
+                            autoComplete="email"
+                            />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField
+                            variant="outlined"
+                            required
+                            fullWidth
+                            name="password"
+                            label="패스워드"
+                            type="password"
+                            id="password"
+                            autoComplete='current-password'
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                            >
+                            로그인
+                        </Button>
+                    </Grid>
+                </Grid>
+            </form>
+        </Container>
     );
 }
 
