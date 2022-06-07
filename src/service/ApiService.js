f6f94db9d3a7b9ef259c9fb28e9536c98ba1daef
Joonwoo Park <93jpark@gmail.com>
2022-06-07 23:34:15 +0900

    edited sigin fuction to store token in localStorage

--- src/service/ApiService.js
+++ src/service/ApiService.js
@@ -39,9 +39,12 @@ export function call(api, method, request) {
 export function signin(userDTO) {
     return call("/auth/signin", "POST", userDTO)
         .then((response)=>{
-            console.log("response : ", response);
-            alert("로그인 토큰 : " + response.token);
+            if(response.token) {
+                // 로컬 스토리지에 토큰 저장
+                localStorage.setItem("ACCESS_TOKEN", response.token);
+                // token이 존재하는 경우 Todo 화면으로 리다이렉트
+                window.location.href="/";
+            }
         })
     }
 
-    
