164b25843e503ac10a71cd45ecc2585bba5b2cc9
Joonwoo Park <93jpark@gmail.com>
2022-06-09 00:55:56 +0900

    implements signout function in ApiSerive

--- src/service/ApiService.js
+++ src/service/ApiService.js
@@ -1,10 +1,20 @@
 import { API_BASE_URL } from "../api-config";
+const ACCESS_TOKEN = "ACCESS_TOKEN";
 
 export function call(api, method, request) {
-    const options = {
-        headers: new Headers({
-            "Content-Type": "application/json",
-        }),
+    
+    let headers = new Headers({
+        "Content-Type": "application/json",
+    });
+
+    // 로컬 스토리지에서 ACCESS_TOKEN 가져오기
+    const accessToken = localStorage.getItem("ACCESS_TOKEN");
+    if(accessToken && accessToken != null) {
+        headers.append("Authorization", "Bearer " + accessToken);
+    }
+
+    let options = {
+        headers: headers,
         url: API_BASE_URL + api,
         method: method,
     };
@@ -30,13 +40,11 @@ export function call(api, method, request) {
             }
             return Promise.reject(json);
         })
-        )
-        
-    
+    )
 }
 
 
-export function signin(userDTO) {
+export const signin = (userDTO) => {
     return call("/auth/signin", "POST", userDTO)
         .then((response)=>{
             if(response.token) {
@@ -45,6 +53,12 @@ export function signin(userDTO) {
                 // token이 존재하는 경우 Todo 화면으로 리다이렉트
                 window.location.href="/";
             }
-        })
-    }
+        }
+    )
+}
+
+export const signout = () => {
+    localStorage.setItem(ACCESS_TOKEN, null);
+    window.location.href = "/login";
+}
 
